463a77f1eb6048b4c9b300917d876bac6099b18f
pat killean <pat.killean@gmail.com>
2024-12-07 16:02:38 -0400

    wip

--- src/dh.clj
+++ src/dh.clj
@@ -1,50 +1,5 @@
 (ns dh)
 
-
-#! TODO
-;java/src/datahike/java/Datahike.java
-;java/src/datahike/java/DatahikeTest.java
-;libdatahike/src/datahike/impl/LibDatahike.java
-;src/datahike/api.cljc
-;src/datahike/api/macros.clj
-;src/datahike/api/specification.cljc
-;src/datahike/array.cljc
-;src/datahike/cli.clj
-;src/datahike/config.cljc
-;src/datahike/connector.cljc
-
-;src/datahike/db/search.cljc
-;src/datahike/db/transaction.cljc
-;src/datahike/experimental/gc.cljc
-;src/datahike/experimental/versioning.cljc
-;src/datahike/impl/entity.cljc
-;src/datahike/lru.cljc
-;src/datahike/middleware/utils.cljc
-;src/datahike/pull_api.cljc
-;src/datahike/readers.cljc
-;src/datahike/schema.cljc
-;src/datahike/schema_cache.cljc
-;src/datahike/store.cljc
-;src/datahike/writer.cljc
-;src/datahike/writing.cljc
-;test/datahike/test/gc_test.cljc
-
-
-;src/datahike/index.cljc
-
-
-#! DONE -------------------------------------------------------
-;deps.edn
-;shadow-cljs.edn
-;src/datahike/datom.cljc
-;src/datahike/index/interface.cljc
-
-
-
-
-
-
-
 ;{:deps {:aliases [:cljs]}
 ; :builds
 ; {
--- src/dh/entry.cljs
+++ src/dh/entry.cljs
@@ -1,6 +1,40 @@
 (ns dh.entry
   (:require [datahike.datom :as datom]
-            [datahike.index.interface]))
+            [datahike.index.interface]
+            [datahike.index.persistent-set :as pss]
+            [datahike.tools]))
 
-(defn -main []
-  (js/console.log "entry start"))
+(defn -main [] (js/console.log "entry start"))
+
+#! TODO
+;src/datahike/api.cljc
+;src/datahike/api/macros.clj
+;src/datahike/api/specification.cljc
+;src/datahike/array.cljc
+;src/datahike/cli.clj
+;src/datahike/config.cljc
+;src/datahike/connector.cljc
+;src/datahike/db/search.cljc
+;src/datahike/db/transaction.cljc
+;src/datahike/experimental/gc.cljc
+;src/datahike/experimental/versioning.cljc
+;src/datahike/impl/entity.cljc
+;src/datahike/lru.cljc
+;src/datahike/middleware/utils.cljc
+;src/datahike/pull_api.cljc
+;src/datahike/readers.cljc
+;src/datahike/schema.cljc
+;src/datahike/schema_cache.cljc
+;src/datahike/store.cljc
+;src/datahike/writer.cljc
+;src/datahike/writing.cljc
+;test/datahike/test/gc_test.cljc
+;src/datahike/index.cljc
+#! DONE -------------------------------------------------------
+;deps.edn
+;shadow-cljs.edn
+;src/datahike/datom.cljc
+;src/datahike/index/interface.cljc
+;src/datahike/index/persistent-set.cljc
+;src/datahike/tools.cljc
+
